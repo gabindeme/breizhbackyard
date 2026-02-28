54cf2f530d63ba3b54d5916608f364318df74e97
Gabin Demé <gabin.deme@etu.imt-nord-europe.fr>
2026-02-28 02:00:00 +0100

    whooppssss

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,15 @@ import "./styles/index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./providers/theme-provider";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <BrowserRouter>
         <App />
-        <Analytics />
       </BrowserRouter>
     </ThemeProvider>
+    <Analytics />
   </StrictMode>,
 );
